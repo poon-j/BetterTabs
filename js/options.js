1ca259bad3d6b8b150e1d6bd2a8bb76e589c1657
Jonathan-Poon <poonj@bxscience.edu>
2020-05-07 03:15:12 -0400

    Option page works!

---
+++ js/options.js
@@ -0,0 +1,39 @@
+function save() {
+	var enableMaxTabs = document.getElementById('maxTabsToggle').checked;
+	var maxTabs = document.getElementById('maxTabCount').value;
+	chrome.storage.sync.set({
+		maxTabsToggle: enableMaxTabs,
+		maxTabCount: maxTabs
+	}, displaySavedMessage);
+}
+
+function displaySavedMessage() {
+	var status = document.getElementById('status');
+	status.textContent = 'Preferences saved.';
+	setTimeout(function() {
+		status.textContent = '';
+	}, 2000);
+}
+
+function load() {
+	chrome.storage.sync.get({
+		maxTabsToggle: false,
+		maxTabCount: '5'
+	}, function(items) {
+		document.getElementById('maxTabsToggle').checked = items.maxTabsToggle;
+		document.getElementById('maxTabCount').value = items.maxTabCount;
+	});
+	updateMaxTabOptions();
+}
+
+function updateMaxTabOptions() {
+	var options = document.getElementById('maxTabsOptions');
+	if (document.getElementById('maxTabsToggle').checked)
+		options.style.display = 'block';
+	else
+		options.style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', load);
+document.getElementById('save').addEventListener('click', save);
+document.getElementById('maxTabsToggle').addEventListener('change', updateMaxTabOptions);
